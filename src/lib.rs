84e5db0b94cb55f80618094c623f7fbe6677e847
4t145 <u4t145@163.com>
2025-09-08 02:56:53 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use http::Method;
 use oas3::spec;
 use quote::{ToTokens, quote};
@@ -8,10 +10,28 @@ pub fn build_from_spec(spec: oas3::Spec) -> Result<(), spec::Error> {
     Ok(())
 }
 
-// pub fn build_schema_type(
-//     spec: spec::Spec,
-//     body: &spec::ObjectSchema,
-// ) -> Result<syn::Item, spec::Error> {
+pub fn build_schema_type(
+    spec: spec::Spec,
+    body: &spec::ObjectSchema,
+) -> Result<syn::Item, spec::Error> {
+    Ok(())
+}
 
-//     Ok(())
-// }
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ItemType {
+    TypeDefinition { type_name: String },
+}
+
+pub struct Builder {
+    spec: oas3::Spec,
+    built_types: HashMap<ItemType, syn::Item>,
+}
+
+impl Builder {
+    pub fn build_schema_type(
+        &mut self,
+        body: &spec::ObjectSchema,
+    ) -> Result<(), spec::Error> {
+
+    }
+}
